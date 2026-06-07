dfe04b9f9c77312ac6b91143f57abeacd2a0868b
DominicFaraday <dominic_faraday@yahoo.co.uk>
2026-06-07 22:46:20 +0100

    EF training and minimal API

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,6 +4,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        
+        
+        
         Console.WriteLine("Hello, World!");
     }
 }
---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var db = new BloggingContext();
+
+        db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+        await db.SaveChangesAsync();
+        var blog = await db.Blogs.OrderBy(b => b.BlogId).FirstAsync();
+        
+        blog.Url = "https://devblogs.microsoft.com/dotnet";
+        blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+        await db.SaveChangesAsync();
+        db.Remove(blog);
+        await db.SaveChangesAsync();
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
